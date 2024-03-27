6fb600807a9654a98769f70834f27a3d752a300a
abdulloh240310gmail.com <abdulloh.turaev@icloud.com>
2024-03-27 13:34:56 +0500

    Hello World

--- src/main/java/example/PlayMovie.java
+++ src/main/java/example/PlayMovie.java
@@ -1,4 +1,13 @@
 package example;
 
 public class PlayMovie {
+    private Movie movie;
+
+    PlayMovie(Movie movie){
+        this.movie = movie;
+    }
+
+    public void Player(){
+        System.out.println(movie.getMovie() + " is being played");
+    }
 }
