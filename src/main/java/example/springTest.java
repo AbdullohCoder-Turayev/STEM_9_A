a69a74e953e386464319601d4e7f5f914437af75
abdulloh240310gmail.com <abdulloh.turaev@icloud.com>
2024-03-29 13:34:51 +0500

    Hello World

--- src/main/java/example/springTest.java
+++ src/main/java/example/springTest.java
@@ -2,8 +2,10 @@ package example;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
+
 public class springTest {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         testBean tb = context.getBean("testBean", testBean.class);
 
